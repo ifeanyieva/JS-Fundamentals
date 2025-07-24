a4319afda7c35eda6964beb6e6c088c8a716ee54
ifeanyieva <evailomuanya95@gmail.com>
2025-07-24 19:04:40 +0100

    modifications again

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,8 +1,14 @@
-const names = ['no argument','Argument found','Arguments found']
+const names = ['no argument','Argument found']
 
-console.log(names[0]);
+console.log(names[0])
+
+const names01 = ['Arguments found','Argument found']
 
 console.log(names[1]);
 
-console.log(names[2]);
+const names02 = ['no argument','Argument found','Arguments found']
+
+console.log(names02[2]);
+
+
 
