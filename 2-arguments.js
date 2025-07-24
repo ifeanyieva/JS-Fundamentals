c0e2dbe2e97039169ff8e38454aee33f44206d90
ifeanyieva <evailomuanya95@gmail.com>
2025-07-24 18:02:44 +0100

    new sentences

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,4 +1,20 @@
-const names = ['','Ada','zeebo']
+const names = ['']
 
 
+if (names.includes('')){
+console.log('No argument')}
+
+
+const names01 = ['Ad']
+
+
+if (names01.includes('d')){
+console.log('Argument found')}
+
+
+const names02 = ['zeebo']
+
+
+if (names02.includes('b')){
+console.log('Arguments found')}
 
